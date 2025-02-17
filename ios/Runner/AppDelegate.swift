c5a7670c0d9802c7cbac8e1fcd39ff6dbfd97459
Antonio Figueiredo <antonio_figueiredo@web.de>
2025-02-17 22:29:04 +0100

    Fix bug: path_provider not working

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,3 +1,18 @@
+// import Flutter
+// import UIKit
+
+// @main
+// @objc class AppDelegate: FlutterAppDelegate {
+//   override func application(
+//     _ application: UIApplication,
+//     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+//   ) -> Bool {
+//     GeneratedPluginRegistrant.register(with: self)
+//     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+//   }
+// }
+
+
 import UIKit
 import Flutter
 
@@ -14,6 +29,7 @@ import Flutter
             // Handle the case where the registrar is nil
             print("Failed to get registrar for GoNativeBridge")
         }
+        GeneratedPluginRegistrant.register(with: self)
         
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,29 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'dart:io';
 import 'package:flutter/services.dart' show rootBundle, MethodChannel;
+import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(SaleNotifierApp());
 }
 
 class SaleNotifierApp extends StatelessWidget {
+  const SaleNotifierApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Sale Notifier',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: GameListScreen(),
     );
   }
 }
 
 class GameListScreen extends StatefulWidget {
+  const GameListScreen({super.key});
+
   @override
   _GameListScreenState createState() => _GameListScreenState();
 }
@@ -42,29 +46,38 @@ class _GameListScreenState extends State<GameListScreen> {
   // Load the game data from the JSON file
   Future<void> _loadGames() async {
     try {
-      String contents = await loadAsset();
+      final directory = await getApplicationDocumentsDirectory();
+      File file = File("$directory/game_list.json"); // await _localFile;
+      print("File path: ${file.path}");
 
-      final String gameInfo = await platform.invokeMethod('getInformation', {
+      // Check if the file exists, if not create it
+      if (!await file.exists()) {
+        await file.create(recursive: true);
+        // Optionally, write initial content to the file
+        await file.writeAsString('[]');
+      }
+
+      await platform.invokeMethod('writeEntry', {
+        "jsonFileName": file.path,
         "url":
-            "https://www.nintendo.com/de-de/Spiele/Nintendo-Switch-Spiele/Donkey-Kong-Country-Returns-HD-2590475.html"
+            "https://www.nintendo.com/de-de/Spiele/Nintendo-Switch-Spiele/Donkey-Kong-Country-Returns-HD-2590475.html",
       });
 
-      // Debugging: print the contents of the file
-      // print("File contents: $contents");
-      print("Game info: $gameInfo");
+      String contents = await file.readAsString();
 
       // Parse the JSON string into a list of game objects
       List<dynamic> gameList = json.decode(contents);
 
       setState(() {
         // Convert the list of game data into a list of maps
-        games = gameList.map((game) {
-          return {
-            'name': game['gameTitle'],
-            'price': game['price'],
-            'saleStatus': game['saleStatus'],
-          };
-        }).toList();
+        games =
+            gameList.map((game) {
+              return {
+                'name': game['GameTitle'],
+                'price': game['DiscountedPrice'],
+                'saleStatus': game['IsDiscounted'],
+              };
+            }).toList();
       });
     } catch (e) {
       print("Error loading game data: $e");
@@ -74,16 +87,15 @@ class _GameListScreenState extends State<GameListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Sale Notifier'),
-      ),
+      appBar: AppBar(title: Text('Sale Notifier')),
       body: ListView.builder(
         itemCount: games.length,
         itemBuilder: (context, index) {
           return ListTile(
             title: Text(games[index]['name'] ?? 'Unknown Game'),
             subtitle: Text(
-                "Price: ${games[index]['price'] ?? 'N/A'}\nSale Status: ${games[index]['saleStatus'] ?? 'N/A'}"),
+              "Price: ${games[index]['price'] ?? 'N/A'}\nSale Status: ${games[index]['saleStatus'] ?? 'N/A'}",
+            ),
             leading: Icon(Icons.videogame_asset),
           );
         },
